d43daec78d7d594cb53c080a6222074e5a885ac6
aoi-tumugi <vitaminchiruchiru@gmail.com>
2021-11-14 12:03:33 +0000

    [Finish]2-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,47 @@
 class BooksController < ApplicationController
+ 
+  def index
+    @book = Book.new
+    @books = Book.all  
+  end
+    
+  def show
+  @book = Book.find(params[:id])
+  end
+  
+   def edit
+    @book = Book.find(params[:id])
+    @books = Book.all
+   end
+   
+   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to edit_book_path(@book.id),notice:"successfully"
+    else
+    render :edit
+    end
+   end 
+  
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+    redirect_to books_path(book.id),notice:"successfully"
+    end
+  end
+
+ def create
+   @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id),notice:"successfully"
+    else
+    @books = Book.all
+   render :index
+    end
+ end
+
+  private
+    def book_params
+    params.require(:book).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  get 'top' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  root :to => 'homes#top'
+
 end
